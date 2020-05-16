88adb6a3b821ad99a988fc7f9366a7f6a5586407
Jen E <jenevans3ten@gmail.com>
2020-05-16 16:31:09 -0400

    passed tests for existence of remaining properties

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -7,6 +7,10 @@ namespace VirtualPet
     public class Pet
     {
         public string Name { get; set; }
+        public string Species { get; set; }
+        public int Hunger { get; set; }
+        public int Boredom { get; set; }
+        public int Health { get; set; }
 
         //private string name;
 
